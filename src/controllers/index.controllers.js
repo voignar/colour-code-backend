e313bd1b7c3fe4ac844bd363bc766e006f5093ed
Israel Cabo <voignar@gmail.com>
2023-08-19 23:40:32 +0200

    update de votos hecho

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -14,7 +14,15 @@ const getCodigos = async (req, res) => {
 }
 
 const createCodigo = async (req, res) => {
+    const { codigo, puntuacion } = req.body;
+    const respuesta1 = await pool.query("SELECT * FROM codigos WHERE codigo = $1", [codigo]);
+    if (respuesta1.rowCount > 0) {
+        await pool.query("UPDATE codigos SET puntuacion = puntuacion + $1, votos = votos + 1 WHERE codigo = $2", [puntuacion, codigo]);
+    }else{
+        await pool.query('INSERT INTO codigos (codigo, puntuacion, votos) VALUES ($1, $2, 1)', [codigo, puntuacion]);
+    }
     
+    res.send("usuario creado");
 }
 
 module.exports = {
